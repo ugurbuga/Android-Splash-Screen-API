929a502778e343a2e57bed85eae50f50482d98e5
Uğur Buğa <47065142+UgurBuga@users.noreply.github.com>
2022-05-06 14:16:04 +0300

    splash screen api example
    splash screen api example

---
+++ app/src/main/java/com/ugurbuga/splashscreenapi/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.ugurbuga.splashscreenapi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
+        setContentView(R.layout.activity_main)
+    }
+}
